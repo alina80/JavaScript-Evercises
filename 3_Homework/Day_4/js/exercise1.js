46064df51d79e40e922230e1f1227faf746bb903
alina80 <alinamateiii@gmail.com>
2020-07-14 07:22:29 +0300

    commit 14.07

--- 3_Homework/Day_4/js/exercise1.js
+++ 3_Homework/Day_4/js/exercise1.js
@@ -5,12 +5,44 @@ $(document).ready(function () {
     btns.eq(0).on('click', function (event) {
         if (spinnerValue.val() <= $('#max').val() - $('#step').val()) {
             spinnerValue.attr('value', parseInt(spinnerValue.attr('value')) + parseInt($('#step').val()));
+            $('.progressInner').attr('style','width:' + ((spinnerValue.val() / $('#max').val()) * 100) + '%');
         }
     });
     btns.eq(1).on('click', function (event) {
         if (spinnerValue.val() >= $('#min').val() + $('#step').val()) {
             spinnerValue.attr('value', parseInt(spinnerValue.attr('value')) - parseInt($('#step').val()));
+            $('.progressInner').attr('style','width:' + ((spinnerValue.val() / $('#max').val()) * 100) + '%');
         }
+    });
+
+    btns.eq(0).on('mousedown',function (event) {
+        var interval = setInterval(function () {
+            if (spinnerValue.val() <= $('#max').val() - $('#step').val()) {
+                spinnerValue.attr('value', parseInt(spinnerValue.attr('value')) + parseInt($('#step').val()));
+                $('.progressInner').attr('style','width:' + ((spinnerValue.val() / $('#max').val()) * 100) + '%');
+            }
+
+        },500);
+        $(this).on('mouseup', function (event) {
+            clearInterval(interval);
+
+        });
+
+    });
+
+    btns.eq(1).on('mousedown',function (event) {
+        var interval = setInterval(function () {
+            if (spinnerValue.val() >= $('#min').val() + $('#step').val()) {
+                spinnerValue.attr('value', parseInt(spinnerValue.attr('value')) - parseInt($('#step').val()));
+                $('.progressInner').attr('style','width:' + ((spinnerValue.val() / $('#max').val()) * 100) + '%');
+            }
+
+        },500);
+        $(this).on('mouseup', function (event) {
+            clearInterval(interval);
+
+        });
+
     });
 
     var saveBtn = $('.saveSettings');
